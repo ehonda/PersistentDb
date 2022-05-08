a555bdf8aa12134559c0e65317520208ef930667
Dennis Renz <dennis@r-renz.de>
2022-05-08 20:21:14 +0200

    More benchmarks

---
+++ PersistentDb.Benchmarks/BenchmarksNoDbAccess.cs
@@ -0,0 +1,50 @@
+﻿using System.Reflection;
+using BenchmarkDotNet.Attributes;
+using Microsoft.EntityFrameworkCore;
+
+namespace PersistentDb.Benchmarks;
+
+public class BenchmarksNoDbAccess
+{
+    public static int Count { get; private set; }
+    
+    private static DbContextOptions<MovieDbContext> EmptyOptions { get; } =
+        new DbContextOptionsBuilder<MovieDbContext>().Options;
+
+    private static ConstructorInfo MovieDbContextConstructor { get; }
+        = typeof(MovieDbContext).GetConstructor(new[] { typeof(DbContextOptions<MovieDbContext>) })!;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        Count = 0;
+    }
+    
+    [Benchmark]
+    public void InvokeConstructor()
+    {
+        using var context = (MovieDbContext) MovieDbContextConstructor.Invoke(new object?[] { EmptyOptions });
+
+        Count += context.ContextId.Lease;
+    }
+    
+    [Benchmark]
+    public void Reflection()
+    {
+        var derivedContextType = typeof(MovieDbContext);
+        var dbContextOptionsType = typeof(DbContextOptions<MovieDbContext>);
+
+        var derivedContextConstructor = derivedContextType.GetConstructor(new[] { dbContextOptionsType });
+        using var context = (MovieDbContext) derivedContextConstructor!.Invoke(new object?[] { EmptyOptions });
+
+        Count += context.ContextId.Lease;
+    }
+    
+    [Benchmark(Baseline = true)]
+    public void DirectConstruction()
+    {
+        using var context = new MovieDbContext(EmptyOptions);
+
+        Count += context.ContextId.Lease;
+    }
+}
--- PersistentDb.Benchmarks/BenchmarksRaw.cs
+++ PersistentDb.Benchmarks/BenchmarksRaw.cs
@@ -1,5 +1,5 @@
-﻿using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Engines;
+﻿using System.Reflection;
+using BenchmarkDotNet.Attributes;
 using Microsoft.EntityFrameworkCore;
 
 namespace PersistentDb.Benchmarks;
@@ -7,39 +7,120 @@ namespace PersistentDb.Benchmarks;
 public class BenchmarksRaw
 {
     public static int Count { get; private set; }
+
+    private static DbContextOptions<MovieDbContext> InMemoryOptions { get; } =
+        OptionsFromConnectionString("DataSource=:memory:");
+
+    private static DbContextOptions<MovieDbContext> FileBasedOptions { get; } =
+        OptionsFromConnectionString("DataSource=test.db");
     
-    private static DbContextOptions<MovieDbContext> DbContextOptions { get; } =
+    private static DbContextOptions<MovieDbContext> EmptyOptions { get; } =
+        new DbContextOptionsBuilder<MovieDbContext>().Options;
+
+    private static ConstructorInfo MovieDbContextConstructor { get; }
+        = typeof(MovieDbContext).GetConstructor(new[] { typeof(DbContextOptions<MovieDbContext>) })!;
+
+    private static DbContextOptions<MovieDbContext> OptionsFromConnectionString(string connectionString) =>
         new DbContextOptionsBuilder<MovieDbContext>()
-            .UseSqlite("DataSource=:memory:")
+            .UseSqlite(connectionString)
             .Options;
 
     [GlobalSetup]
     public void Setup()
     {
         Count = 0;
+        using var context = new MovieDbContext(FileBasedOptions);
+        context.Database.EnsureDeleted();
+        context.Database.EnsureCreated();
     }
 
     [Benchmark]
-    public void Reflection()
+    public void InvokeConstructorInMemory()
     {
-        var derivedContextType = typeof(MovieDbContext);
-        var dbContextOptionsType = typeof(DbContextOptions<MovieDbContext>);
-        
-        var derivedContextConstructor = derivedContextType.GetConstructor(new[] { dbContextOptionsType });
-        using var context = (MovieDbContext) derivedContextConstructor!.Invoke(new object?[] { DbContextOptions });
-        
+        using var context = (MovieDbContext) MovieDbContextConstructor.Invoke(new object?[] { InMemoryOptions });
+
         context.Database.OpenConnection();
         context.Database.EnsureCreated();
         Count += context.Movies.Count();
     }
 
-    [Benchmark(Baseline = true)]
-    public void DirectConstruction()
+    [Benchmark]
+    public void InvokeConstructorFileBased()
     {
-        using var context = new MovieDbContext(DbContextOptions);
-        
+        using var context = (MovieDbContext) MovieDbContextConstructor.Invoke(new object?[] { FileBasedOptions });
+
+        Count += context.Movies.Count();
+    }
+    
+    [Benchmark]
+    public void InvokeConstructorNoDatabaseAccess()
+    {
+        using var context = (MovieDbContext) MovieDbContextConstructor.Invoke(new object?[] { EmptyOptions });
+
+        Count += context.ContextId.Lease;
+    }
+
+    [Benchmark]
+    public void ReflectionInMemory()
+    {
+        var derivedContextType = typeof(MovieDbContext);
+        var dbContextOptionsType = typeof(DbContextOptions<MovieDbContext>);
+
+        var derivedContextConstructor = derivedContextType.GetConstructor(new[] { dbContextOptionsType });
+        using var context = (MovieDbContext) derivedContextConstructor!.Invoke(new object?[] { InMemoryOptions });
+
         context.Database.OpenConnection();
         context.Database.EnsureCreated();
         Count += context.Movies.Count();
+    }
+
+    [Benchmark]
+    public void ReflectionFileBased()
+    {
+        var derivedContextType = typeof(MovieDbContext);
+        var dbContextOptionsType = typeof(DbContextOptions<MovieDbContext>);
+
+        var derivedContextConstructor = derivedContextType.GetConstructor(new[] { dbContextOptionsType });
+        using var context = (MovieDbContext) derivedContextConstructor!.Invoke(new object?[] { FileBasedOptions });
+
+        Count += context.Movies.Count();
+    }
+    
+    [Benchmark]
+    public void ReflectionNoDatabaseAccess()
+    {
+        var derivedContextType = typeof(MovieDbContext);
+        var dbContextOptionsType = typeof(DbContextOptions<MovieDbContext>);
+
+        var derivedContextConstructor = derivedContextType.GetConstructor(new[] { dbContextOptionsType });
+        using var context = (MovieDbContext) derivedContextConstructor!.Invoke(new object?[] { EmptyOptions });
+
+        Count += context.ContextId.Lease;
+    }
+
+    [Benchmark]
+    public void DirectConstructionInMemory()
+    {
+        using var context = new MovieDbContext(InMemoryOptions);
+
+        context.Database.OpenConnection();
+        context.Database.EnsureCreated();
+        Count += context.Movies.Count();
+    }
+
+    [Benchmark]
+    public void DirectConstructionFileBased()
+    {
+        using var context = new MovieDbContext(FileBasedOptions);
+
+        Count += context.Movies.Count();
+    }
+    
+    [Benchmark(Baseline = true)]
+    public void DirectConstructionNoDatabaseAccess()
+    {
+        using var context = new MovieDbContext(EmptyOptions);
+
+        Count += context.ContextId.Lease;
     }
 }
