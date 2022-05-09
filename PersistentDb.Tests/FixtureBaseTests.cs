f98d748e03a85de9c5d4764bb22986bef77afa9e
Dennis Renz <dennis@r-renz.de>
2022-05-09 23:00:46 +0200

    Check stuff about db creation / deletion

---
+++ PersistentDb.Tests/FixtureBaseTests.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using FluentAssertions;
+using Microsoft.Data.Sqlite;
+using NUnit.Framework;
+using PersistentDb.TestUtilities;
+
+namespace PersistentDb.Tests;
+
+public class FixtureBaseTests
+{
+    [Test]
+    public void SetUp_creates_file()
+    {
+        var fixture = new TestFixtureWithDbContext<MovieDbContext>();
+        fixture.SetUpContext();
+        File.Exists("test.db").Should().BeTrue();
+    }
+    
+    [Test]
+    public void TearDown_deletes_file()
+    {
+        var fixture = new TestFixtureWithDbContext<MovieDbContext>();
+        fixture.SetUpContext();
+        fixture.TearDownContext();
+        File.Exists("test.db").Should().BeFalse();
+    }
+
+    [Test]
+    public void Db_File_is_locked()
+    {
+        using var fileStream = new FileStream("test.db", FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None);
+
+        var fixture = new MovieFixture();
+        
+        var creation = () =>
+        {
+            using var context = fixture.CreateContext();
+            return context.Database.EnsureCreated();
+        };
+
+        creation.Should().Throw<SqliteException>();
+    }
+}
---
+++ PersistentDb.Tests/MovieFixture.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using PersistentDb.TestUtilities;
+
+namespace PersistentDb.Tests;
+
+public class MovieFixture : TestFixtureWithDbContext<MovieDbContext>
+{
+    public new Func<MovieDbContext> CreateContext => base.CreateContext;
+}
