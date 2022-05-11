0961b6b1dbee3b7981bd3cbc6079b0af7958fe57
Dennis Renz <dennis@r-renz.de>
2022-05-11 23:16:14 +0200

    Add test fixture with parameterized test

---
+++ PersistentDb.Tests/ParameterizedTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace PersistentDb.Tests;
+
+public static class Foo
+{
+    public static string Bar<T>(T t) => "Don't care";
+}
+
+public class ParameterizedTests
+{
+    private static class ValueSources
+    {
+        public static IEnumerable<int> Integers => new[] { 0, 1, 2 };
+
+        public static IEnumerable<char> Characters => new[] { 'a', 'b', 'c' };
+    }
+
+    [Test]
+    public void FooBar_does_not_care<T>(
+        [ValueSource(typeof(ValueSources), nameof(ValueSources.Integers))]
+        [ValueSource(typeof(ValueSources), nameof(ValueSources.Characters))]
+        T input)
+    {
+        Foo.Bar(input).Should().Be("Don't care");
+    }
+}
